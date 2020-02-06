a8c4c0883606db15a8dd1b63b0188d634d57e487
jozhe <joze.kosmerl@gmail.com>
2020-02-06 21:59:50 +0800

    Remove unnecessary logs

--- lib/docker_stake_service/bitly/bitly_client.ex
+++ lib/docker_stake_service/bitly/bitly_client.ex
@@ -11,9 +11,9 @@ defmodule DockerStakeService.BitlyClient do
       long_url: Application.get_env(:docker_stake_service, :url_domain) <> "poll/" <> poll_id
     } |> Poison.encode!()
 
-    with true <- Application.get_env(:docker_stake_service, :enable_bitly) |> IO.inspect(),
+    with true <- Application.get_env(:docker_stake_service, :enable_bitly),
          headers <- @common_headers ++ [{"Authorization", "Bearer " <> Application.get_env(:docker_stake_service, :bitly_token)}],
-         {:ok, %{body: body}} <- @endpoint <> "shorten" |> HTTPoison.post(body, headers) |> IO.inspect(),
+         {:ok, %{body: body}} <- @endpoint <> "shorten" |> HTTPoison.post(body, headers),
          %{"link" => link} <- body |> Poison.decode!() do
       {:ok, link}
     else
